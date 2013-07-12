0175591fd437b90484a2bb1f685fc9b6aa1f8528
blueShade <dante4002@gmail.com>
2013-07-12 13:57:09 +0530

    Adding first project first cut

---
+++ src/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author piusharma
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello!!");
+	}
+
+}
